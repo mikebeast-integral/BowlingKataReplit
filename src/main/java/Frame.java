7c78c51b10c13563b7ecc95a593f5c859f200f5f
MichaelBeasterf <>
2021-12-20 17:51:36 +0000

    Initial commit

---
+++ src/main/java/Frame.java
@@ -0,0 +1,25 @@
+public class Frame {
+    private String moves;
+    public Frame(String moves) {
+        this.moves = moves;
+    }
+
+    public int calculateScore() {
+        if (moves.equals("X")) {
+            return 10;
+        }
+
+        if (moves.charAt(1) == '/') {
+            return 10;
+        }
+
+        moves = moves.replaceAll("-","0");
+
+        int turnOne = Character.getNumericValue(moves.charAt(0));
+        int turnTwo = Character.getNumericValue(moves.charAt(1));
+
+        return  turnOne + turnTwo;
+
+        //return 0;
+    }
+}
